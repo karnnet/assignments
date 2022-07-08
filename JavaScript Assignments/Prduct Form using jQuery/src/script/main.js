339cbcf85110ad12eb017023cdc5a2fad3ddbe93
navneet <knavneet@qualminds.com>
2022-07-08 13:42:25 +0530

    Added CSS to Product Cart and Table

--- JavaScript Assignments/Prduct Form using jQuery/src/script/main.js
+++ JavaScript Assignments/Prduct Form using jQuery/src/script/main.js
@@ -44,35 +44,50 @@ $(() => {
         event.stopPropagation();
     });
 
-    // When quantity changes in product list, update array with changed quantity and re-render lists.
-    $('tbody').on('change', 'tr td input', function(event) {
-        var quantityIndex = $(this).parents('tr').index();
-        var productUpdate = products[quantityIndex];
-        productUpdate.quantity = $(this).val();
-        clearProductList();
-        renderProductList();
-        renderProductCart();
-        resetForm();
-        event.stopPropagation();
-    });
+    // // When quantity changes in product list input, update array with changed quantity and re-render lists.
+    // $('tbody').on('change', 'tr td input', function(event) {
+    //     var quantityIndex = $(this).parents('tr').index();
+    //     var productUpdate = products[quantityIndex];
+    //     productUpdate.quantity = $(this).val();
+    //     clearProductList();
+    //     renderProductList();
+    //     renderProductCart();
+    //     resetForm();
+    //     event.stopPropagation();
+    // });
 
-    //adding quantity 
-    $('tbody').on('click', 'tr td .add', function(event) {
+
+    // When quantity changes in product list, update array with changed quantity and re-render lists.
+    // $('tbody').on('change', 'tr td input', function(event) {
+    //     var quantityIndex = $(this).parents('tr').index();
+    //     var productUpdate = products[quantityIndex];
+    //     productUpdate.quantity = $(this).val();
+    //     clearProductList();
+    //     renderProductList();
+    //     renderProductCart();
+    //     resetForm();
+    //     event.stopPropagation();
+    // });
+
+    //adding quantity value 
+    $('#product_list').on('click', 'tr td div .add', function(event) {
         var productIndexPlus = $(this).parents('tr').index();
         var productUpdateValue = products[productIndexPlus];
-        productUpdateValue.quantity = parseInt($(this).siblings('input').val()) + 1;
+        productUpdateValue.quantity = parseInt($(this).siblings('span').text()) + 1;
         clearProductList();
         renderProductList();
         renderProductCart();
         event.stopPropagation();
     });
 
-    //decreasing quantity and removing row when quantity is zero
-    $('tbody').on('click', 'tr td .minus', function(event) {
+    //decreasing quantity and removing row from index when quantity equals to zero
+    $('#product_list').on('click', 'tr td div .minus', function(event) {
         var productIndexMinus = $(this).parents('tr').index();
-        var productUpdateValue = products[productIndexMinus];
-        productUpdateValue.quantity = parseInt($(this).siblings('input').val()) - 1;
-        if (productUpdateValue.quantity == 0) {
+        var selectedProduct = products[productIndexMinus];
+        var productQuantity = parseInt($(this).siblings('span').text()) - 1;
+        if (productQuantity > 0) {
+            selectedProduct.quantity = productQuantity;
+        } else {
             products.splice(productIndexMinus, 1);
         }
         clearProductList();
@@ -104,11 +119,23 @@ $(() => {
         if (productName == '' || productQuantity == '' || productPrice == '') {
             alert('Product detail input is required.');
             return false;
+        } else if (productName.match(/ /g)) {
+            alert('No whitespaces allowed');
+            return false;
+        } else if (!Number(productQuantity)) {
+            alert('Enter only number');
+            return false;
+        } else if (productQuantity < 0) {
+            alert("Quantity can't be negative");
+            return false;
+        } else if (productQuantity.match(/[^0-9\.]/g)) {
+            alert("Quantity can't be decimal value");
+            return false;
         } else if (!Number(productPrice)) {
             alert('Enter only number');
             return false;
-        } else if (productName.match(/ /g)) {
-            alert('No whitespaces allowed');
+        } else if (productPrice < 0) {
+            alert("Price can't be negative");
             return false;
         } else if (productPrice.match(/ /g)) {
             alert('No whitespaces allowed');
@@ -123,7 +150,7 @@ $(() => {
         products.forEach(function(product) {
             $('#product_list').append(`<tr>
             <td class='product-name'>${product.name}</td>
-            <td class='product-quantity'><i class="fa fa-minus minus"></i><input type="text" value="${product.quantity}"/><i class="fa fa-plus add"></i></td>
+            <td class='product-quantity'><div class="cart-quantity"><i class="fa fa-minus minus"></i><span class="value">${product.quantity}</span><i class="fa fa-plus add"></i></div></td>
             <td class='product-price'>${product.price}</td>
             <td class='product-subtotal'>${product.quantity*product.price}</td>
             <td><i class="fa-solid fa-trash align-right delete"></i></td>
